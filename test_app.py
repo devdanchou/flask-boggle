b7390c703b30034213641266106ef1f256c42f3c
Belle Xu <bellex22@protonmail.com>
2023-07-21 19:03:46 -0400

    test json

--- test_app.py
+++ test_app.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 from app import app, games
 
+
 # Make Flask errors be real errors, not HTML pages with error info
 app.config['TESTING'] = True
 
@@ -23,12 +24,21 @@ class BoggleAppTestCase(TestCase):
 
         with self.client as client:
             response = client.get('/')
-            ...
+            html = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('<button class="word-input-btn">Go</button>', html)
             # test that you're getting a template
 
     def test_api_new_game(self):
         """Test starting a new game."""
 
         with self.client as client:
-            ...
-            # write a test for this route
+            response = client.post("/api/new-game")
+            json_data = response.get_json()
+            # dict_from_json = json.loads(json_data)
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(response.is_json, True)
+            self.assertIn("gameId", json_data)
+            self.assertIn("board", json_data)
+            breakpoint()
+            self.assertIn(json_data["gameId"], games)
